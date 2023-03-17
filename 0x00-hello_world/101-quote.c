4a3d51d419a95bce94a017733e340c081c278ddd
Yahya Achrane <yahya.achrane12@gmail.com>
2023-03-17 04:49:59 -0700

    Advanced 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <unistd.h>
-
+/**
+ *main - Entry point of the code
+ *void - main function take no arguments
+ *Return:1-(Success)
+ */
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
