44df2c0e6350579f0f418adf07b39d10619006b0
Yahya Achrane <yahya.achrane12@gmail.com>
2023-03-16 18:37:10 -0700

    Sidi Ali

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,21 +2,21 @@
 /**
  *main - Entry point of the code
  *void - main function take no arguments
- *Return: Always 0 (Success)
+ *Return:0-(Success)
  */
 
 int main(void)
 {
-	int myInt;
-	char myChar;
-	long int myLongInt;
-	float myFloat;
-	long  long myLongLongInt;
+	int a;
+	char b;
+	long int c;
+	float d;
+	long  long e;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(myChar));
-	printf("Size of an int: %lu byte(s)\n", sizeof(myInt));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(myLongInt));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(myLongLongInt));
-	printf("Size of a float: %lu byte(s)\n", sizeof(myFloat));
+	printf("Size of a char: %lu byte(s)\n", sizeof(b));
+	printf("Size of an int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(e));
+	printf("Size of a float: %lu byte(s)\n", sizeof(d));
 	return (0);
 }
