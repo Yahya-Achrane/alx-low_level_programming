cbbe0d4a189e93e20ad264dc99dab562294d5874
Yahya Achrane <yahya.achrane12@gmail.com>
2023-03-16 18:07:39 -0700

    Level up

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int myInt;
+	char myChar;
+	long int myLongInt;
+	float myFloat;
+	long myLongLongInt;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(myChar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(myInt));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(myLongInt));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(myLongLongInt));
+	printf("Size of a float: %lu byte(s)\n", sizeof(myFloat));
+	return (0);
+}
