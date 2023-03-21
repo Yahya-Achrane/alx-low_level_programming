738c7b97fe3113682dbc57b8226e49ad346228a5
Yahya Achrane <yahya.achrane12@gmail.com>
2023-03-21 16:53:10 -0700

    Test2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,21 @@
   *
   * Return: Always 0 (secusess)
   */
-int main()
+int _putchar(char c)
 {
-	write(1, "_putchar\n", 9 );
-	return(0);
+	return (write(1, &c, 1));
+}
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
