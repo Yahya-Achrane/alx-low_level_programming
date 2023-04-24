690ca2a5fd36f228829e8f63def201080ff72d7e
Yahya Achrane <yahya.achrane12@gmail.com>
2023-04-23 17:17:31 -0700

    ..

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,62 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_string - print
+ * @n: string
+ * return: none if n == NULL, otherwise return n
+ */
+
+void print_string(char *n)
+{
+	if (n == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", n);
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list type;
+	char *s_val, c_val;
+	int i_val, i;
+	float f_val;
+
+	va_start(type, format);
+	i = 0;
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				c_val = va_arg(type, int);
+				printf("%c", c_val);
+				break;
+			case 'i':
+				i_val = va_arg(type, int);
+				printf("%d", i_val);
+				break;
+			case 'f':
+				f_val = va_arg(type, double);
+				printf("%f", f_val);
+				break;
+			case 's':
+				s_val = va_arg(type, char *);
+				print_string(s_val);
+				break;
+		}
+		if (format[i - 1] && format[i + 1])
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
+	va_end(type);
+}
