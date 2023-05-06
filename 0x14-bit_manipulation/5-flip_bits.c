1f783f7ad17040f130aff0a9800c5a1b57e5944a
Yahya Achrane <yahya.achrane12@gmail.com>
2023-05-05 18:59:26 -0700

    let's goo

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would need to flip to get from one number to another.
+ * flip_bits - returns the number of bits we need to flip  number to another.
  * @n: the first number
  * @m: the second number
- * Return: the number of bit flips needed to transform one binary string into another
+ * Return: the number of bit flips needed to transform one binary str
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
